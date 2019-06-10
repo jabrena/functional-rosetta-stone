e124d39a6547aedf1c188fc12d1dceb0ef943225
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-10 18:43:44 +0200

    Adding solution for Java & Java Streams

--- src/main/java/org/fundamentals/fp/euler/problem3/EulerProblem3.java
+++ src/main/java/org/fundamentals/fp/euler/problem3/EulerProblem3.java
@@ -1,5 +1,16 @@
 package org.fundamentals.fp.euler.problem3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.LongConsumer;
+import java.util.stream.LongStream;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
+
 /**
  * https://projecteuler.net/problem=3
  *
@@ -23,8 +34,57 @@ package org.fundamentals.fp.euler.problem3;
  */
 public class EulerProblem3 {
 
+    private List<Long> primeFactors(long number) {
+        long n = number;
+        List<Long> factors = new ArrayList<>();
+        for (long i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        return factors;
+    }
+
     public long javaSolution(long number) {
 
-        return 0L;
+        List<Long> factorList = primeFactors(number);
+        return factorList.get(factorList.size() -1);
+    }
+
+    static private LongStream primeFactorsIntStream(long n) {
+        int characteristics = Spliterator.ORDERED | Spliterator.SORTED | Spliterator.IMMUTABLE | Spliterator.NONNULL;
+        Spliterator.OfLong spliterator = new Spliterators.AbstractLongSpliterator(Long.MAX_VALUE, characteristics) {
+            long val = n;
+            int div = 2;
+
+            @Override
+            public boolean tryAdvance(LongConsumer action) {
+                while (div <= val) {
+                    if (val % div == 0) {
+                        action.accept(div);
+                        val /= div;
+                        return true;
+                    }
+                    div += div == 2 ? 1 : 2;
+                }
+                return false;
+            }
+
+            @Override
+            public Comparator<? super Long> getComparator() {
+                return null;
+            }
+        };
+        return StreamSupport.longStream(spliterator, false);
+    }
+
+    private List<Long> primeFactorsStream(long number) {
+        return primeFactorsIntStream(number).boxed().collect(toList());
+    }
+
+    public long javaStreamSolution(long number) {
+
+        return primeFactorsStream(number).stream().reduce((first, second) -> second).get();
     }
 }
--- src/test/java/org/fundamentals/fp/euler/problem3/EulerProblem3Test.java
+++ src/test/java/org/fundamentals/fp/euler/problem3/EulerProblem3Test.java
@@ -16,4 +16,14 @@ public class EulerProblem3Test extends BaseEulerProblemTest {
 
     }
 
+    @Test
+    public void given_primeFactorsStream_when_13195_then_expectedResultsTest(){
+
+        EulerProblem3 problem3 = new EulerProblem3();
+
+        AssertionsForClassTypes.assertThat(problem3.javaStreamSolution(13195L)).isEqualTo(29);
+        AssertionsForClassTypes.assertThat(problem3.javaStreamSolution(600851475143L)).isEqualTo(euler.getAnswerToLong(3));
+
+    }
+
 }
