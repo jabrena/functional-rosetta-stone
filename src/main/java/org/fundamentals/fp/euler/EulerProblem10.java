e53805191e0156e9bcb65de0c394a9961021ab85
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-21 09:27:52 +0200

    Java Stream Solution

--- src/main/java/org/fundamentals/fp/euler/EulerProblem10.java
+++ src/main/java/org/fundamentals/fp/euler/EulerProblem10.java
@@ -1,8 +1,25 @@
 package org.fundamentals.fp.euler;
 
+import java.util.stream.LongStream;
+import org.apache.commons.lang3.NotImplementedException;
+
 public class EulerProblem10 {
 
+    public Long javaSolution(long limit) {
+
+        throw new NotImplementedException("¯\\_(ツ)_/¯");
+    }
+
+    boolean isPrime(long number) {
+        return LongStream.rangeClosed(2, number / 2)
+                .noneMatch(i -> number % i == 0);
+    }
+
     public long javaStreamSolution(long limit) {
-        return 0;
+         return LongStream.iterate(1, i -> i + 1)
+                .filter(this::isPrime)
+                .skip(1)
+                .takeWhile(number -> number <= limit)
+                .sum();
     }
 }
--- src/test/java/org/fundamentals/fp/euler/EulerProblem10Test.java
+++ src/test/java/org/fundamentals/fp/euler/EulerProblem10Test.java
@@ -8,11 +8,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class EulerProblem10Test extends BaseEulerProblemTest {
 
     @Test
-    public void given_problem6_when_10_then_expectedResultsTest(){
+    public void given_problem10_when_10_then_expectedResultsTest(){
 
         EulerProblem10 problem = new EulerProblem10();
 
         assertThat(problem.javaStreamSolution(10L)).isEqualTo(17);
+        //Working but very slow in execution
         assertThat(problem.javaStreamSolution(2_000_000L)).isEqualTo(euler.getAnswerToLong(10));
     }
 
