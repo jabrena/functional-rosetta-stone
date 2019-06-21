1a96a48ee73001a77d0d0dd4954de56d1b42f763
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-21 15:37:49 +0200

    Adding Java Streams solution

---
+++ src/main/java/org/fundamentals/fp/euler/EulerProblem20.java
@@ -0,0 +1,44 @@
+package org.fundamentals.fp.euler;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import scala.reflect.internal.Chars;
+
+/**
+ * Problem 20: Factorial digit sum
+ * n! means n (n 1) ... 3 2 1
+ *
+ * For example, 10! = 10   9   ...   3   2   1 = 3628800,
+ *
+ * and the sum of the digits in the number 10! is
+ * 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ * Find the sum of the digits in the number 100!
+ *
+ */
+public class EulerProblem20 {
+
+    Function<Long, BigInteger> factorial = limit -> LongStream.iterate(limit, i -> i - 1)
+            .limit(limit)
+            .mapToObj(BigInteger::valueOf)
+            .reduce((n1, n2) -> n1.multiply(n2))
+            .get();
+
+    Function<BigInteger, List<String>> toChars = value -> value.toString().chars()
+            .mapToObj(c -> String.valueOf((char) c))
+            .collect(Collectors.toList());
+
+    Function<List<String>, Long> sum = chars -> chars.stream()
+            .mapToLong(s -> Long.valueOf(s))
+            .reduce((l1, l2) -> l1 + l2)
+            .getAsLong();
+
+    public long javaStreamSolution(long limit) {
+        return factorial.andThen(toChars).andThen(sum).apply(limit);
+    }
+}
---
+++ src/test/java/org/fundamentals/fp/euler/EulerProblem20Test.java
@@ -0,0 +1,23 @@
+package org.fundamentals.fp.euler;
+
+import java.math.BigInteger;
+import org.assertj.core.api.BigIntegerAssert;
+import org.fundamentals.fp.euler.utils.BaseEulerProblemTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class EulerProblem20Test extends BaseEulerProblemTest {
+
+    @Test
+    public void given_problem16_when_javaStreamSolution_then_expectedResultsTest(){
+
+        EulerProblem20 problem = new EulerProblem20();
+
+        assertThat(problem.factorial.apply(10L)).isEqualTo(BigInteger.valueOf(3628800));
+        assertThat(problem.javaStreamSolution(10L)).isEqualTo(27L);
+        assertThat(problem.javaStreamSolution(100L)).isEqualTo(euler.getAnswerToLong(20));
+    }
+
+
+}
