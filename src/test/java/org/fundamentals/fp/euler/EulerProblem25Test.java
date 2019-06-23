563507d537808cb8ad199e7c6cc34e8def6fd0d0
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-23 18:56:38 +0200

    Addinng initial test for Problem 25

---
+++ src/test/java/org/fundamentals/fp/euler/EulerProblem25Test.java
@@ -0,0 +1,18 @@
+package org.fundamentals.fp.euler;
+
+import org.fundamentals.fp.euler.utils.BaseEulerProblemTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class EulerProblem25Test extends BaseEulerProblemTest {
+
+    @Test
+    public void given_problem_when_javaStreamSolution_then_expectedResultsTest(){
+
+        EulerProblem25 problem = new EulerProblem25();
+
+        assertThat(problem.javaStreamSolution(1000L)).isEqualTo(euler.getAnswerToLong(25));
+    }
+
+}
