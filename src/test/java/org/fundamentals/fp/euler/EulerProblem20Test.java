bf38e33823a2edf0d7be85bac1d14cb2e3e31585
Juan Antonio Brena Moral <bren@juanantonio.info>
2019-06-26 17:58:39 +0200

    Adding initial tests for Problem 34

--- src/test/java/org/fundamentals/fp/euler/EulerProblem20Test.java
+++ src/test/java/org/fundamentals/fp/euler/EulerProblem20Test.java
@@ -1,6 +1,5 @@
 package org.fundamentals.fp.euler;
 
-import java.math.BigInteger;
 import org.fundamentals.fp.euler.utils.BaseEulerProblemTest;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +12,6 @@ public class EulerProblem20Test extends BaseEulerProblemTest {
 
         EulerProblem20 problem = new EulerProblem20();
 
-        assertThat(problem.factorial.apply(10L)).isEqualTo(BigInteger.valueOf(3628800));
         assertThat(problem.javaStreamSolution(10L)).isEqualTo(27L);
         assertThat(problem.javaStreamSolution(100L)).isEqualTo(euler.getAnswerToLong(20));
     }
