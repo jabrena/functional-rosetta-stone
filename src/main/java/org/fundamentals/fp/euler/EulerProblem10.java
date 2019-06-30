b793a10fcef12e8e6dd92574589220c8405c0773
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-30 22:24:23 +0200

    Minor refactoring

--- src/main/java/org/fundamentals/fp/euler/EulerProblem10.java
+++ src/main/java/org/fundamentals/fp/euler/EulerProblem10.java
@@ -1,5 +1,6 @@
 package org.fundamentals.fp.euler;
 
+import java.util.function.Predicate;
 import java.util.stream.LongStream;
 import org.apache.commons.lang3.NotImplementedException;
 
@@ -10,14 +11,13 @@ public class EulerProblem10 {
         throw new NotImplementedException("¯\\_(ツ)_/¯");
     }
 
-    boolean isPrime(long number) {
-        return LongStream.rangeClosed(2, number / 2)
+    Predicate<Long> isPrime = number ->
+            LongStream.rangeClosed(2, number / 2)
                 .noneMatch(i -> number % i == 0);
-    }
 
     public long javaStreamSolution(long limit) {
          return LongStream.iterate(1, i -> i + 1)
-                .filter(this::isPrime)
+                .filter(isPrime::test)
                 .skip(1)
                 .takeWhile(number -> number <= limit)
                 .sum();
