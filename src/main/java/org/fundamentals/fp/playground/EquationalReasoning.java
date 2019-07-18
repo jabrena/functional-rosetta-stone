27adf439a927d40b949aab14acdeb1d456372b13
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-07-18 09:43:33 +0200

    Adding initial example for equational reasoning

---
+++ src/main/java/org/fundamentals/fp/playground/EquationalReasoning.java
@@ -0,0 +1,20 @@
+package org.fundamentals.fp.playground;
+
+import io.vavr.Function2;
+import java.util.List;
+import java.util.function.Function;
+
+public class EquationalReasoning {
+
+    public static void main(String[] args) {
+
+        var list = List.of(80, 90, 100);
+
+        Function2<Integer, Integer, Integer> divide = (i1, i2) -> i2 / i1;
+        Function<List<Integer>, Integer> sum = l ->
+                l.stream().reduce(0, (i1, i2) -> i1 + i2);
+
+        System.out.println(divide.apply(sum.apply(list), list.size()));
+    }
+
+}
