cb6e3956439bcf735a4421f9901c514d0ddb2b6c
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-07-18 17:27:09 +0200

    Adding closures

---
+++ src/test/java/org/fundamentals/fp/euler/EulerProblem67Test.java
@@ -0,0 +1,46 @@
+package org.fundamentals.fp.euler;
+
+import org.fundamentals.fp.euler.utils.BaseEulerProblemTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EulerProblem67Test extends BaseEulerProblemTest implements IEulerTestable   {
+
+    @Override
+    public void given_JavaSolution_when_executeMethod_then_expectedResultsTest() {
+
+    }
+
+    @Override
+    public void given_JavaStreamSolution_when_executeMethod_then_expectedResultsTest() {
+
+    }
+
+    @Disabled
+    @Test
+    @Override
+    public void given_VAVRSolution_when_executeMethod_then_expectedResultsTest() {
+
+        EulerProblem67 problem = new EulerProblem67();
+        //then(problem.smart(100)).isEqualTo(euler.getAnswerToLong(67));
+    }
+
+    @Override
+    public void given_ReactorSolution_when_executeMethod_then_expectedResultsTest() {
+
+    }
+
+    @Override
+    public void given_RxJavaSolution_when_executeMethod_then_expectedResultsTest() {
+
+    }
+
+    @Override
+    public void given_KotlinSolution_when_executeMethod_then_expectedResultsTest() {
+
+    }
+}
