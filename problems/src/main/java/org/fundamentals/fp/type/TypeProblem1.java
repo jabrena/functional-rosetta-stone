c578740ca7264782b4c8719674231d97f0d84bf5
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-10-27 16:53:29 +0100

    Initial test for TypeProblem1

--- problems/src/main/java/org/fundamentals/fp/type/TypeProblem1.java
+++ problems/src/main/java/org/fundamentals/fp/type/TypeProblem1.java
@@ -1,13 +1,24 @@
 package org.fundamentals.fp.type;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class TypeProblem1 {
 
+    private String DEFAULT_FALLBACK = "Not available";
     private final Person person;
 
     public String getInsuranceName() {
-        return person.getBeachHouse().getInsurance().getName();
+
+        if(!Objects.isNull(person)) {
+
+            if(!Objects.isNull(person.getBeachHouse())) {
+
+                return person.getBeachHouse().getInsurance().getName();
+            }
+        }
+
+        return DEFAULT_FALLBACK;
     }
 }
--- problems/src/test/java/org/fundamentals/fp/type/TypeProblem1Test.java
+++ problems/src/test/java/org/fundamentals/fp/type/TypeProblem1Test.java
@@ -20,4 +20,16 @@ public class TypeProblem1Test {
         then(problem.getInsuranceName()).isEqualTo(DEFAULT_INSURANCE_NAME);
     }
 
+    @Test
+    public void given_javaStreamSolution_when_PersonDoesntHaveAnyBeachHouse_then_returnExpectedResult() {
+
+        //Given
+        String DEFAULT_FALLBACK = "Not available";
+        Person person = new Person(null);
+
+        TypeProblem1 problem = new TypeProblem1(person);
+
+        then(problem.getInsuranceName()).isEqualTo(DEFAULT_FALLBACK);
+    }
+
 }
