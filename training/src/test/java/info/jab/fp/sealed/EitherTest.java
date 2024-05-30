599c0aef220c5e4f73a768bbdc13a8917da7bb0a
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-05-30 18:24:52 +0000

    Initial examples with Sealed

--- training/src/test/java/info/jab/fp/sealed/EitherTest.java
+++ training/src/test/java/info/jab/fp/sealed/EitherTest.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
-
 package info.jab.fp.sealed;
 
 import org.junit.jupiter.api.BeforeAll;
